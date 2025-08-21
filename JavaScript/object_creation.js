1aa31ba30b71437bf82fea2033256e93f5f04aa7
Kartikay-Dutta <kartikay0512.becse24@chitkara.edu.in>
2025-08-21 10:15:24 +0530

    code

---
+++ JavaScript/object_creation.js
@@ -0,0 +1,10 @@
+const Dell = {
+    brand: "Dell",
+    price: 55000,
+    isAvailable: true
+};
+
+document.writeln(Dell.brand);       
+document.writeln(Dell.price);       
+document.writeln(Dell.isAvailable); 
+3
